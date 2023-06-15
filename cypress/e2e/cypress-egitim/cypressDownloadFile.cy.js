16497fe8266b26f72afe195f97dc5469c6102bae
MUSTAFA ÖZSAMUR <mustafaozsamur22@gmail.com>
2023-06-15 18:35:22 +0200

    15.06/V2

--- cypress/e2e/cypress-egitim/cypressDownloadFile.cy.js
+++ cypress/e2e/cypress-egitim/cypressDownloadFile.cy.js
@@ -1,12 +1,12 @@
 describe('Cypress Download File', () => {
 
-    before(() => {          //uncaught:exception hatasini gidermek icin kod blogu
-        cy.deleteDownloadsFolder() 
+    before(() => {          
+        cy.deleteDownloadsFolder() // Downloads Folder Delete icin
     });
 
 
     it('Download File', () => {
-        cy.on('uncaught:exception',(err,runnable)=>{return false})
+        cy.on('uncaught:exception',(err,runnable)=>{return false})   //uncaught:exception hatasini gidermek icin kod blogu
         cy.visit('https://demoqa.com/upload-download')
         cy.get('#downloadButton').click() // 1.Yöntem
         cy.get('a.btn-primary').click() // 2.Yöntem
